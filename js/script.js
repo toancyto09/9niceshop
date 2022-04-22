e0e81124b72ac8972dc6341bd80f7fb4ec530de6
toancyto09 <101265301+toancyto09@users.noreply.github.com>
2022-04-22 12:53:30 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const topNavOne = document.querySelector('.navbar-nav-right');
+const topNavTwo = document.querySelector('.navbar-nav-left');
+
+const navbarToggler = document.querySelector('.navbar-toggler');
+navbarToggler.addEventListener('click', () => {
+    topNavOne.classList.toggle('showNav');
+    topNavTwo.classList.toggle('showNav');
+});
